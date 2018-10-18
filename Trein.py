7ced1389f1fd77e027198310a1f4a66c45d32de1
FaizalSupriadi <43231255+FaizalSupriadi@users.noreply.github.com>
2018-10-18 14:45:34 +0200

    Update Trein.py

--- Trein.py
+++ Trein.py
@@ -1,21 +1,28 @@
 import requests
 import xmltodict
 from tkinter import *
+from tkinter.messagebox import showinfo
 
 
 root = Tk()
 
-def destroyer(lijst,refresher):
-        lijst[0].destroy()
-        lijst[1].destroy()
-        lijst[2].destroy()
-        lijst[3].destroy()
-        lijst[4].destroy()
-        refresher.destroy()
+def error():
+    bericht = 'Verkeerde invoer!'
+    showinfo(title='Error', message=bericht)
 
-        return begin()
+def destroyer(lijst,refresher):
+    q = 0
+    while True:
+        lijst[q].destroy()
+        q +=1
+        if q == 10:
+            break
+    refresher.destroy()
+
+    return begin()
 
 def clicked(label,entry,button):
+    try:
         q = 0
         stationIn = entry.get() #Verkrijgt het getypte station
         tekst = "Station: {}"
@@ -32,7 +39,7 @@ def clicked(label,entry,button):
 
             Button(master=root, text='Refresh', command=(button.destroy(), label.destroy(), entry.destroy()))
             #De vertrek weten
-            for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein'][:5]:
+            for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein'][:10]:
                 eindbestemming = vertrek['EindBestemming']
 
                 vertrektijd = vertrek['VertrekTijd']  # 2016-09-27T18:36:00+0200
@@ -47,10 +54,14 @@ def clicked(label,entry,button):
                                  foreground='white',
                                  font='comicsansms', )
             refresh.pack(pady=50)
+    except KeyError:
+        error()
+        return begin()
+
 
 
 def begin():
-    label = Label(master=root, text='Kies een station', height = 10, background='#ffd72a', width=100, font='comicsansms')
+    label = Label(master=root, text='Kies een station', height = 10, background='#ffd72a', width=50, font='comicsansms')
     label.pack()
 
     entry = Entry(master=root)
@@ -61,6 +72,7 @@ def begin():
     button.pack(pady=10)
 
     root.configure(background='#ffd72a')
+    root.title('NS treinen')
     root.mainloop()
 
 begin()
