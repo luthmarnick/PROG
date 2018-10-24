d5a350cb8ec4c77be3a1c0228cb1b60fc56d37c1
FaizalSupriadi <43231255+FaizalSupriadi@users.noreply.github.com>
2018-10-24 13:02:23 +0200

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -7,24 +7,27 @@ from email import encoders
 
 
 #Def functie is niet nodig, maar was handig om 'Uit te zetten' met hashtag
+#Je kan input weg doen en daar je standaard Mail, WW of bericht inschrijven ipv te typen. 
 def mail():
     mail = input('Je mail: ')
     ww = input('WW: ')#PAS OP! Het intypen van je WW wordt niet anders weergeven, dus als iemand meekijkt kan die het zien!
 
     msg = MIMEMultipart()
-
+    
+   
     msg['From'] = mail
     msg['To'] = mail
     msg['Subject'] = "SUBJECT OF THE EMAIL"
 
-    body = str(input('Schrijf je bericht: ')) + "\nVerzonden op {}".format(
-        datetime.datetime.now().strftime("%B %d, %Y"))
+    body = str(input('Schrijf je bericht: ')) + "\nVerzonden op {}".format( 
+        datetime.datetime.now().strftime("%B %d, %Y"))#Tijd wanneer verzonden
 
     msg.attach(MIMEText(body, 'plain'))
 
-    filename = 'ticker.txt'#"NAME OF THE FILE WITH ITS EXTENSION"
-    attachment = open('C:/Users/Faiza/Desktop/ticker.txt', "rb")#"PATH OF THE FILE"
-
+    filename = 'Naam van de file'#"NAME OF THE FILE WITH ITS EXTENSION"
+    attachment = open('Waar de file is', "rb")#"PATH OF THE FILE"
+    
+    #codeerd de attachment naar assembly code
     part = MIMEBase('application', 'octet-stream')
     part.set_payload((attachment).read())
     encoders.encode_base64(part)
@@ -36,7 +39,7 @@ def mail():
     server_ssl.ehlo()
     server_ssl.login(mail, ww)
     text = msg.as_string()
-    server_ssl.sendmail(mail, mail, text)
+    server_ssl.sendmail(mail, mail, text)  #Stuurt naar zichzelf
     server_ssl.quit()
 
 mail()
