04ea9e4f909ac4dc4fd4bb23c3cbe10c7ea3b09b
FaizalSupriadi <43231255+FaizalSupriadi@users.noreply.github.com>
2018-10-26 13:57:42 +0200

    Add files via upload

--- gmail.py
+++ gmail.py
@@ -2,42 +2,23 @@ import smtplib
 import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-
 
 # Def functie is niet nodig, maar was handig om 'Uit te zetten' met hashtag
 # Je kan input weg doen en daar je standaard Mail, WW of bericht inschrijven ipv te typen.
 # Als je dit wil gebruiken, moet je bij je mail dit https://myaccount.google.com/lesssecureapps aan zetten
 def mail():
-    mail = 'viphuhl@gmail.com'
-    ww = 'Joh@nHU1'  # PAS OP! Het intypen van je WW wordt niet anders weergeven, dus als iemand meekijkt kan die het zien!
-
+    mail = 'viphuhl@gmail.com'  # PAS OP! Het intypen van je WW wordt niet anders weergeven, dus als iemand meekijkt kan die het zien!
+    ww = 'Joh@nHU1'
     msg = MIMEMultipart()
 
     msg['From'] = mail
     msg['To'] = mail
     msg['Subject'] = input("Mail onderwerp: ")
 
-    body = str(input('Schrijf je bericht: ')) + "\n\nVerzonden op {}".format(
-        datetime.datetime.now().strftime("%B %d, %Y"))  # Tijd wanneer verzonden
-
-    Attach = input('Attachment(ja/nee): ')
+    body = 'Je alarm is afgegaan!' + "\n\nVerzonden op {}".format(
+        datetime.datetime.now().strftime("%d %B %Y %H:%M."))# Tijd wanneer verzonden
 
     msg.attach(MIMEText(body, 'plain'))
-
-    if Attach == 'ja':
-
-        filename = input('Filename: ')  # "NAME OF THE FILE WITH ITS EXTENSION"
-        attachment = open(input('File path: '), "rb")  # "PATH OF THE FILE"
-
-    # codeerd de attachment naar assembly code
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-        msg.attach(part)
 
     server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server_ssl.ehlo()
