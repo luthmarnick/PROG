8a912bf07cf9a3a3a506ecf1b23ca02ec52b4ff9
FaizalSupriadi <43231255+FaizalSupriadi@users.noreply.github.com>
2018-10-26 23:42:11 +0200

    Update Trein.py

--- Trein.py
+++ Trein.py
@@ -3,8 +3,11 @@ import xmltodict
 from tkinter import *
 from tkinter.messagebox import showinfo
 
-
 root = Tk()
+root.maxsize(700, 675)  #zet de maximale grote van de window
+root.minsize(700, 675)  #zet de minimale grote van de window
+img = Image('photo', file='logoNS.png')
+root.call('wm', 'iconphoto', root._w,img) #zet het plaatje als icoon neer
 
 #De def die de clicked() verwijderd en terug zet naar begin()
 def destroyer(lijst,refresher):
@@ -20,7 +23,7 @@ def destroyer(lijst,refresher):
     return begin()
 
 #De def die de vertrektijden en bestemmingen laat zien
-def clicked(label,entry,button):
+def clicked(label,entry,button,stoppen):
     try:
         lijst = []
 
@@ -36,17 +39,20 @@ def clicked(label,entry,button):
             button.destroy()
             label.destroy()
             entry.destroy()
+            stoppen.destroy()
 
             #De vertrek tijden en bestemmingen vinden in de XML File
             for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein'][:10]: #Dit zal zich 10 keer repeaten
                 eindbestemming = vertrek['EindBestemming']
+                spoor = vertrek['VertrekSpoor']['#text'] #geeft het spoor waarop de trein rijd weer
 
                 vertrektijd = vertrek['VertrekTijd']  # 2016-09-27T18:36:00+0200
-                vertrektijd = vertrektijd[11:16]  # 18:36
+                vertrektijd = vertrektijd[11:16]  # 11:16 geeft het tijd format aan
+                #print(vertrektijd['VertrekTijd'])
 
                 #Laat de vertrektijden en eindbestemming zien
-                stationText = Label(master=root,  height=2,text = 'Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming,
-                                    background='#ffd72a', font='comicsansms')
+                stationText = Label(master=root,  height=2,text = 'Om '+vertrektijd+' vertrekt een trein naar '+ eindbestemming +' op spoor ' + spoor,
+                                    background='#ffd72a', font='Comic')
                 #Doet de stationText in de lijst en we gebruiken de lijst om daarmee de teksten te verwijderen
                 lijst.append(stationText)
                 stationText.pack()
@@ -54,23 +60,23 @@ def clicked(label,entry,button):
             #De button die ervoor zorgt dat het terug gaat naar het begin scherm, de '#02339a' is de blauwe NS kleur
             refresh = Button(master=root, text='Refresh', command=lambda: destroyer(lijst, refresh), background='#02339a',
                                  foreground='white',
-                                 font='comicsansms', )
+                                 font='Comic', )
             refresh.pack(pady=50)
 
     #Bij een keyerror zal dit zich uitvoeren
     except KeyError:
         showinfo(title='Error', message='Verkeerde invoer!')#Een pop-up error scherm
         return begin() #Dit zorgt ervoor dat het terug gaat naar het beginscherm
+
     #Bij een andere error zal dit zich uitvoeren
     except:
         showinfo(title='Error', message='Er ging iets verkeerd')
         return begin()
 
-
 #Def begin() is waar het programma start
 def begin():
     #Dit zorgt voor de tekst
-    label = Label(master=root, text='Kies een station', height = 10, background='#ffd72a', width=50, font='comicsansms')
+    label = Label(master=root, text='Kies een station', height = 10, background='#ffd72a', width=50, font='Comic')
     label.pack()
 
     #Hierin kan je de gewenste station schrijven
@@ -78,9 +84,14 @@ def begin():
     entry.pack(padx=10, pady=10)
 
     #De button zorgt ervoor dat het clicked def zal starten
-    button = Button(master=root, text='Plannen', command=lambda: clicked(label,entry,button), background='#02339a',
-                    foreground='white', font='comicsansms', )
-    button.pack(pady=10)
+    button = Button(master=root, text='Plannen', command=lambda: clicked(label,entry,button,stoppen), background='#02339a',
+                    foreground='white', font='Comic', )
+    button.place(x=225,y=250,)
+
+    stoppen = Button(master=root, text='Stoppen', command=root.destroy, background='#02339a', #Stopt het programma
+                    foreground='white', font='Comic', )
+
+    stoppen.place(x=400,y=250)
 
     root.configure(background='#ffd72a')#De NS geel achtergrond
     root.title('NS Treinen') #Veranderd de titel
